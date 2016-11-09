25b693e45b84b0395491936ebd330276ad0998c2
dsladovi <dsladovi@foi.hr>
2016-11-09 18:31:38 +0100

    Klasa DatabaseData
    Dodavanje namirnica u bazu kod prvog pokretanja aplikacije

--- DiabeticCalculator/app/src/main/java/com/air/foi/diabeticcalculatorapp/MainActivity.java
+++ DiabeticCalculator/app/src/main/java/com/air/foi/diabeticcalculatorapp/MainActivity.java
@@ -5,6 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.air.foi.diabeticcalculatorapp.preferences.CustomPreferencesActivity;
+import com.raizlabs.android.dbflow.config.FlowConfig;
+import com.raizlabs.android.dbflow.config.FlowManager;
+import com.raizlabs.android.dbflow.sql.language.SQLite;
+
+import java.util.List;
+
+import entities.DatabaseData;
+import entities.Namirnica;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FlowManager.init(new FlowConfig.Builder(this).build());
+
+        List<Namirnica> listaNnamirnica = SQLite.select()
+                .from(Namirnica.class).queryList();
+
+        if (listaNnamirnica.size() > 0){
+        }
+        else {
+
+            DatabaseData.writeAll();
+        }
     }
 }
---
+++ DiabeticCalculator/app/src/main/java/entities/DatabaseData.java
@@ -0,0 +1,94 @@
+package entities;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Danijel on 9.11.2016..
+ */
+
+public class DatabaseData {
+
+    public static void writeAll(){
+
+        /*
+        TipObroka dorucak = new TipObroka("Doručak");
+        dorucak.save();
+
+        TipObroka rucak = new TipObroka("Ručak");
+        rucak.save();
+
+        TipObroka vecera = new TipObroka("Večera");
+        vecera.save();
+
+        TipMjerenja nataste = new TipMjerenja("Natašte");
+        nataste.save();
+
+        TipMjerenja ostalo = new TipMjerenja("Ostalo");
+        ostalo.save();
+
+        TipMjerenja nakonObroka = new TipMjerenja("Nakon obroka");
+        nakonObroka.save();
+
+        Namirnica pireKrumpir = new Namirnica("Krumpir - pire", 15);
+        pireKrumpir.save();
+
+        Namirnica kuhanaRiza = new Namirnica("Kuhana riža - bijela", 28);
+        kuhanaRiza.save();
+
+        Namirnica pommesFites = new Namirnica("Pommes Frites", 41);
+        pommesFites.save();
+
+        Namirnica spageti = new Namirnica("Spageti", 31);
+        spageti.save();
+
+        Namirnica pohanaPiletina = new Namirnica("Pohani pileći file", 9);
+        pohanaPiletina.save();
+
+        Namirnica pecenaPiletina = new Namirnica("Pečena piletina", 0);
+        pecenaPiletina.save();
+
+        Namirnica pecenaSvinjetina = new Namirnica("Pečena svinjetina", 0);
+        pecenaSvinjetina.save();
+
+        Namirnica banana = new Namirnica("Banana", 22.8);
+        banana.save();
+
+        Namirnica jabuka = new Namirnica("Jabuka", 13.8);
+        jabuka.save();
+
+        Namirnica bijeliKruh = new Namirnica("Bijeli kruh", 49.1);
+        bijeliKruh.save();
+
+        Namirnica salama = new Namirnica("Salame - razne vrste", 2);
+        salama.save();
+
+        Date datum = new Date();
+
+
+        String stringDatum = "2016-10-15";
+        Date datum2 = new Date();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            datum2 = df.parse(stringDatum);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        OstalaMjerenja mjerenje2 = new OstalaMjerenja(datum2, nataste, 6.6);
+        mjerenje2.save();
+
+        String stringDatum2 = "2016-10-16";
+        try {
+            datum2 = df.parse(stringDatum2);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        OstalaMjerenja mjerenje3 = new OstalaMjerenja(datum2, nataste, 8.6);
+        mjerenje3.save();
+
+        OstalaMjerenja mjerenje1 = new OstalaMjerenja(datum, nataste, 5.6);
+        mjerenje1.save();
+            */
+    }
+}
