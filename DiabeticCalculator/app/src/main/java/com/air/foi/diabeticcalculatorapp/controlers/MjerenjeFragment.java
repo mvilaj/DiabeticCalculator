ffad7bc6deb7e15f9ca91685a3fb3601c7cab715
Mario Vilaj <vilaj.mario@gmail.com>
2016-12-08 20:05:13 +0100

    Kreiran UI za unos mjerenja
    Kreiran UI za unos mjerenja vrijednosti glukoze prema mockupu

---
+++ DiabeticCalculator/app/src/main/java/com/air/foi/diabeticcalculatorapp/controlers/MjerenjeFragment.java
@@ -0,0 +1,20 @@
+package com.air.foi.diabeticcalculatorapp.controlers;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+/**
+ * Created by Mario on 12/8/2016.
+ */
+
+public class MjerenjeFragment {
+    private Spinner spTipObroka;
+    private Spinner spTipMjerenja;
+    private Button btnSpremi;
+    private EditText etGuk;
+    private TextView tvObrok;
+
+
+}
